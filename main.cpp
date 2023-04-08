44cd912dc7ca40148d8b6bfa96630a5ada618911
daynlight <danielstodulski000@gmail.com>
2023-04-09 01:45:36 +0200

    Good

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main()
 	Vec* Mapa = Map.GetVertices();
 	for(int i =0;i<4;i++)
 	{
-
 		std::cout << glm::to_string(Mapa[i].Pos) << " " << glm::to_string(Mapa[i].Tex) << " " << glm::to_string(Mapa[i].Nor) << std::endl;
 	}
 	return 0;
