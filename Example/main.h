9a834e0f88bc7495c5f83997db1353524fe2dbf4
daynlight <danielstodulski000@gmail.com>
2023-04-08 03:53:20 +0200

    gf

--- Example/main.h
+++
@@ -1,3 +0,0 @@
-
-#include "LoadOBJ.h"
-#include <iostream>
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	LoadOBJ Map = LoadOBJ("Assets/Map/object.obj");
 	Vec* Mapa = Map.GetVertices();
-	std::cout << Mapa[0].Tex.x << std::endl;
+	std::cout << Mapa[0].Pos.x << std::endl;
 	return 0;
 
 };
