99931ce58d4adcd9efaa1b8f7ff1e9e4e46588ff
daynlight <danielstodulski000@gmail.com>
2023-04-09 04:32:54 +0200

    Oragnizacja

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-﻿
-#include "main.h"
+﻿#include "main.h"
 
 
 int main()
@@ -7,15 +6,11 @@ int main()
 	LoadOBJ Map = LoadOBJ("Assets/Map/object.obj");
 	VectexArray* Mapa = Map.Vertices();
 	int* Mapind = Map.Indices();
-	for(int i =0;i<Map.VerticesSize;i++)
-	{
-		std::cout << glm::to_string(Mapa[i].Poses) << " " << glm::to_string(Mapa[i].Texures) << " " << glm::to_string(Mapa[i].Normals) << std::endl;
-	}
-	for (int i = 0; i < Map.IndicesSize; i++)
-	{
-		std::cout << Mapind[i] << std::endl;
-	}
-	return 0;
+	for(int i =0;i<Map.VerticesSize;i++) std::cout 
+	<< glm::to_string(Mapa[i].Poses) << " " << glm::to_string(Mapa[i].Texures) 
+	<< " " << glm::to_string(Mapa[i].Normals) << std::endl;
+	for (int i = 0; i < Map.IndicesSize; i++) std::cout << Mapind[i] << std::endl;
 
+	return 0;
 };
 
--- main.h
+++ main.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "LIB/include/GLFW/glfw3.h"
 #include "LIB/include/glad/glad.h"
 #include "LoadOBJ.h"
