3c724a0391ec9840f929859c32b58eb07f6931ba
Student <Student@DESKTOP-TAIOGUV>
2023-04-19 12:41:20 +0200

    Memory Mangment

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-	LoadOBJ Map = LoadOBJ("Assets/Map/object.obj");
-	VectexArray* Mapa = Map.VertexData;
-	GLuint* Mapind = Map.Indices;
-	for(int i =0;i<Map.VerticesSize;i++) std::cout 
+	LoadOBJ* Map = new LoadOBJ("Assets/Map/object.obj");
+	VectexArray* Mapa = Map->VertexData;
+	GLuint* Mapind = Map->Indices;
+	for(int i =0;i<Map->VerticesSize;i++) std::cout 
 	<< glm::to_string(Mapa[i].Poses) << " " << glm::to_string(Mapa[i].Texures) 
 	<< " " << glm::to_string(Mapa[i].Normals) << std::endl;
-	for (int i = 0; i < Map.IndicesSize; i++) std::cout << Mapind[i] << std::endl;
-
+	for (int i = 0; i < Map->IndicesSize; i++) std::cout << Mapind[i] << std::endl;
+	delete Map;
 	return 0;
 };
 
